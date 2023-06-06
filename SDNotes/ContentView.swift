ba6378062164594fda3ac3276cc7653364ca7843
Ishaan Bedi <bediishaan6@gmail.com>
2023-06-06 20:42:41 +0530

    Initial Commit

---
+++ SDNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDNotes
+//
+//  Created by Ishaan Bedi on 06/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDNotes/SDNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDNotesApp.swift
+//  SDNotes
+//
+//  Created by Ishaan Bedi on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SDNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
